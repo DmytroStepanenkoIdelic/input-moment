1d00cbad68bd0a1d450095a3bdb28f1974730284
sugarman <sugarman@google.com>
2017-08-10 11:11:26 -0700

    update gh-pages 2017-08-10T18:11:26.422Z

--- app.js
+++ app.js
@@ -1,51 +1,52 @@
-require('../src/less/input-moment.less');
-require('./app.less');
+require('../src/less/datetime-picker.less')
+require('./app.less')
 
-var moment = require('moment');
-var React = require('react');
-var ReactDOM = require('react-dom');
-var InputMoment = require('../src/input-moment');
-var packageJson = require('../package.json');
+var moment = require('moment')
+var React = require('react')
+var ReactDOM = require('react-dom')
+var DatetimePicker = require('../src/datetime-picker')
+var packageJson = require('../package.json')
 
 var App = React.createClass({
   displayName: 'App',
 
   getInitialState() {
     return {
-      m: moment()
-    };
+      m: moment(),
+    }
   },
 
   render() {
     return (
       <div className="app">
-        <h1>{packageJson.name}</h1>
-        <h2>{packageJson.description}</h2>
+        <h1>
+          {packageJson.name}
+        </h1>
+        <h2>
+          {packageJson.description}
+        </h2>
         <form>
-        <div className="input">
-          <input
-            type="text"
-            value={this.state.m.format('llll')}
-            readOnly
+          <div className="input">
+            <input type="text" value={this.state.m.format('llll')} readOnly />
+          </div>
+          <DatetimePicker
+            moment={this.state.m}
+            onChange={this.handleChange}
+            type="datetime"
+            onDone={this.handleDone}
           />
-        </div>
-        <InputMoment
-          moment={this.state.m}
-          onChange={this.handleChange}
-          type="datetime"
-        />
         </form>
       </div>
-    );
+    )
   },
 
   handleChange(m) {
-    this.setState({ m });
+    this.setState({ m })
   },
 
-  handleSave() {
-    console.log('saved', this.state.m.format('llll'));
-  }
-});
+  handleDone() {
+    console.log('saved', this.state.m.format('llll'))
+  },
+})
 
-ReactDOM.render(<App/>, document.getElementById('app'));
+ReactDOM.render(<App />, document.getElementById('app'))
